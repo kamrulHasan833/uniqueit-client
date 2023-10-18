63bd52ceef5dc8e120198a49e5c91bc0d80ea961
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-10-18 11:59:21 +0600

    initial setup added

--- hooks/useIsUser.js
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "../components/contexts/authContext";
-
-function useIsUser() {
-  const { user } = useAuth();
-  const isUser = Object.keys(user).length === 0 ? false : true;
-  return isUser;
-}
-export default useIsUser;
